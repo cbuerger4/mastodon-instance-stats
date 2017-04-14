c41123cce66f45f48de8a76c5a5eebf16ecf121e
jemus42 <jemus42@quantenbrot.de>
2017-04-14 03:12:57 +0200

    Better tooting

--- toot_instance.R
+++ toot_instance.R
@@ -5,6 +5,7 @@ library(jsonlite)
 library(dplyr)
 library(readr)
 library(stringr)
+library(mastodon)
 
 instance <- read_rds("data/mastodon.rds") %>%
   filter(time == max(time, na.rm = TRUE)) %>%
@@ -14,9 +15,28 @@ diceroll <- ceiling(as.numeric(format(Sys.time(), "%s"))/60) %% nrow(instance)
 instance <- instance %>% slice(sample(diceroll, 1))
 
 toot <- paste0(instance$instance,
-               " (", instance$users, " User(s), ",
-               instance$toots, " Toots) is currently ",
+               "\n", instance$users, " User(s), ",
+               instance$toots, " Toots\n…is currently ",
                str_to_upper(instance$open_reg), "!")
-toot <- paste0("'", toot, "'")
 
-system(command = paste("/usr/local/bin/toot", toot))
+mastodon %>%
+  filter(instance == instance$instance) %>%
+  ggplot(data = ., aes(x = time, y = users, fill = open_reg)) +
+  geom_path() +
+  geom_point(shape = 21, colour = "black", size = 2, stroke = .5) +
+  labs(title = "Instance User History",
+       subtitle = instance$instance,
+       x = "Date", y = "User #", fill = "Registrations",
+       caption = format(Sys.time(), '%F %H:%M (%Z)')) +
+  tadaatoolbox::theme_readthedown() +
+  theme(legend.position = "top") -> plot
+
+## Toot stuff out
+
+credentials <- jsonlite::fromJSON("~/.config/mastodon-rstats.json")
+
+token = login(credentials$url, credentials$user, credentials$pass)
+
+#post_status(token, toot)
+
+post_ggplot(token, toot, plot)
